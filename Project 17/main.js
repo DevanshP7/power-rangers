50ad094af523325093f95cf884213b73cb039a04
DevanshP7 <79050720+DevanshP7@users.noreply.github.com>
2021-08-03 15:22:59 +0530

    Add files via upload

---
+++ Project 17/main.js
@@ -0,0 +1,60 @@
+canvas = new fabric.Canvas("Canvas");
+
+block_y=1;
+block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+function new_image(get_image){
+
+	fabric.Image.fromURL(get_image, function(Img){
+    block_image_object = Img;
+
+	block_image_object.scaleToHeight(block_image_height);
+	block_image_object.scaleToWidth(block_image_width);
+	block_image_object.set({
+    top:block_y,
+	left:block_x
+	});
+	canvas.add(block_image_object);
+	});
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e){
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '82'){
+		block_x = 100
+		new_image("rr1.png");
+		console.log("Red Ranger");
+	}
+
+	if(keyPressed == '71'){
+		block_x = 200;
+		new_image("greenpic.png");
+		console.log("Green Ranger");
+	}
+	
+	if(keyPressed == '89'){
+		block_x = 350;
+		new_image("yr.png");
+		console.log("Yellow Ranger");
+	}
+
+	if(keyPressed == '80'){
+		block_x = 600;
+		new_image("pr.png");
+		console.log("Pink Ranger");
+	}
+
+	if(keyPressed == '66'){
+		block_x = 700;
+		new_image("1111.png");
+		console.log("Blue Ranger");
+	}
+	
+}
